70e516b9bae5945bc97e8a8655a67cc2fb320bea
Anastasia <Grebcova>
2023-12-11 12:11:21 +0300

    done

--- lr6/pg3.sql
+++ lr6/pg3.sql
@@ -1,15 +1,16 @@
 /*Выберите процент использования объектов по месяцам, упорядочив по возрастанию*/
 USE cd;
-WITH slots as (SELECT facility, SUM(b.slots) AS Стоимость_аренды,   
-DATE_FORMAT(b.starttime, '%y %m') AS Год_Месяц
-FROM facilities AS f
-JOIN bookings AS b ON b.facid = f.facid
-GROUP BY f.facid, Год_Месяц
+
+WITH slots as (
+  SELECT facility, SUM(book.slots) as rent_count,
+      DATE_FORMAT(book.starttime, "%m %Y") as date
+    FROM facilities as fac
+      JOIN bookings as book ON book.facid = fac.facid
+    GROUP BY fac.facid, date
 )
-SELECT sl1.facility AS 'Объекты', 
-ROUND(sl1.Стоимость_аренды / SUM(sl2.Стоимость_аренды) * 100, 1) AS Процент_использования_объектов_в_месяц,
-sl1.Год_Месяц
-FROM slots AS sl1
-JOIN slots AS sl2 ON sl1.Год_Месяц = sl2.Год_Месяц
-GROUP BY sl1.facility, sl1.Стоимость_аренды, sl1.Год_Месяц
-ORDER BY Процент_использования_объектов_в_месяц ASC;
+SELECT s1.facility, CONCAT(ROUND(s1.rent_count / SUM(s2.rent_count) * 100, 1), '%') as usability,
+    s1.date
+  FROM slots as s1
+    JOIN slots as s2 ON s1.date = s2.date
+  GROUP BY s1.facility, s1.rent_count, s1.date
+  ORDER BY CAST(usability as FLOAT) ASC;
